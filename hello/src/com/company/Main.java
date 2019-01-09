9e41cbd5d25a1c46434be18d43552846911c8a30
dingyangzhuang <13950056074@139.com>
2019-01-09 16:35:29 +0800

    the first
    Signed-off-by: dingyangzhuang <13950056074@139.com>

---
+++ hello/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        /**
+         * 1.读取文件，获取IP列表
+         * 2.登录数据库，查看对应IP的所在的设备
+         * 3.使用ssh登录设备进行ping操作
+         */
+        System.out.println("hah");
+    }
+}
